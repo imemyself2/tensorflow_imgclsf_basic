3545481ef21fabe9e001769ee4d36701fd43c65c
imemyself2 <dkaneria@purdue.edu>
2020-01-05 12:40:55 -0500

    Keras model complete. Optimize to reduce memory consumption

--- imgclsf_keras.py
+++ imgclsf_keras.py
@@ -1,5 +1,21 @@
 from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.layers import MaxPooling2D
+from img_input import imginput
 
 model = Sequential()
 
-model.add()
+X_train, Y_train = imginput("D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\dogs", "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\cats")
+
+model.add(Conv2D(64, (8, 8), padding='same', activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (4, 4), padding='same', activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(1, activation='softmax'))
+
+model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=50, epochs=2)
