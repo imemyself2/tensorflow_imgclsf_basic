3bb54b0b9bf25574928c1308df277f19370b2696
imemyself2 <dkaneria@purdue.edu>
2020-01-06 00:51:25 -0500

    Model overfits. Add regularization/Change network structure.

--- img_input.py
+++ img_input.py
@@ -4,8 +4,9 @@ from sklearn.utils import shuffle
 import os
 import cv2
 
+IMG_SIZE = 112
 def imginput(DOGDATADIR, CATDATADIR):
-    IMG_SIZE = 112
+    
     dog_arr = []
     cat_arr = []
 
@@ -64,3 +65,63 @@ def imginput(DOGDATADIR, CATDATADIR):
     X_train, Y_train = shuffle(X_train_orig, Y_train_orig, random_state = 0)
     X_train = X_train/225
     return X_train, Y_train
+
+def load_test_img(DOGTESTIMG, CATTESTIMG):
+    dog_arr = []
+    cat_arr = []
+
+    dog_data = np.empty(shape=(1012, 112, 112))
+    cat_data = np.empty(shape=(1011, 112, 112))
+
+
+    try:
+        dogpath = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\dogsTestSaved"
+        catpath = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\catsTestSaved"
+        dogs = open(dogpath, 'rb')
+        cats = open(catpath, 'rb')
+        dog_data = np.load(dogs)
+        cat_data = np.load(cats)
+        print('Existing save files found....')
+    except IOError:
+        
+        for img in os.listdir(DOGTESTIMG):
+            try: 
+                dog_img = cv2.imread(os.path.join(DOGTESTIMG, img), cv2.IMREAD_GRAYSCALE)
+                dog_img = cv2.resize(dog_img, (IMG_SIZE, IMG_SIZE))
+                dog_arr.append(dog_img)
+            except:
+                continue
+        
+        for img in os.listdir(CATTESTIMG):
+            try:
+                cat_img = cv2.imread(os.path.join(CATTESTIMG, img), cv2.IMREAD_GRAYSCALE)
+                cat_img = cv2.resize(cat_img,(IMG_SIZE, IMG_SIZE))
+                cat_arr.append(cat_img)
+            except:
+                continue
+        
+
+        dog_data = np.asanyarray(dog_arr)
+        cat_data = np.asanyarray(cat_arr)
+        dog_data = np.expand_dims(dog_data, axis=4)
+        cat_data = np.expand_dims(cat_data, axis=4)
+
+        outfile = open(dogpath, 'wb')
+        np.save(outfile, dog_data)
+        outfile.close()
+        outfile2 = open(catpath, 'wb')
+        np.save(outfile2, cat_data)
+        outfile2.close()
+        print("save files created for future runs.")
+
+
+
+    dog_label = np.ones((dog_data.shape[0], 1))
+    cat_label = np.zeros((cat_data.shape[0],1))
+
+    X_test_orig = np.concatenate((dog_data, cat_data), axis = 0)
+    Y_test_orig = np.concatenate((dog_label, cat_label), axis = 0)
+
+    X_test, Y_test = shuffle(X_test_orig, Y_test_orig, random_state = 0)
+    X_test = X_test/225
+    return X_test, Y_test
--- imgclsf_keras.py
+++ imgclsf_keras.py
@@ -5,15 +5,47 @@ from keras.layers import Flatten
 from keras.layers import Input
 from keras.layers import MaxPooling2D
 from img_input import imginput
+from img_input import load_test_img
+from keras.layers import BatchNormalization
+from keras.layers import Activation
+from keras.models import load_model
+import matplotlib.pyplot as plt
 
-model = Sequential()
+# Paths to different files
+model_save_dest = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\gitcode\\modelV1.h5"
+model_save_weights_dest = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\gitcode\\modelV1_weights.h5"
+dog_test_dir = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\test_set\\test_set\\dogs"
+cat_test_dir = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\test_set\\test_set\\cats"
+dog_train_dir = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\dogs"
+cat_train_dir = "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\cats"
 
-X_train, Y_train = imginput("D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\dogs", "D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\training_set\\training_set\\cats")
+# Loading/Creating a model
+model = None
+try:
+    model = load_model("D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\gitcode\\modelV1.h5")
 
-model.add(Conv2D(3, (8, 8), padding='same', activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+    model.load_weights("D:\\Work\\College\\Spring 2020\\DeepLearningCoursera-2\\gitcode\\modelV1_weights.h5")
+    print("model and weights loaded successfully.")
+except:
+    X_train, Y_train = imginput(dog_train_dir, cat_train_dir)
 
-model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, Y_train, batch_size=200, epochs=50)
+    model = Sequential()
+    model.add(Conv2D(3, (8, 8), padding='same', activation=None))
+    model.add(BatchNormalization(axis=3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(X_train[0:4000], Y_train[0:4000], batch_size=32, epochs=40)
+
+    model.save(model_save_dest)
+    model.save_weights(model_save_weights_dest)
+
+## Predict a custom image below
+testimages, testlabels = load_test_img(dog_test_dir, cat_test_dir)
+result = model.evaluate(x=testimages, y=testlabels, batch_size=64, verbose=1)
+
+print("Loss: "+str(result[0]))
+print("Accuracy: "+str(result[1]))
